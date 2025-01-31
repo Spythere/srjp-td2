518b2da7001fea1129f92492e0469a324456de3a
Spythere <spythere@protonmail.com>
2025-01-31 02:11:09 +0100

    chore: i18n setup

---
+++ src/i18n.ts
@@ -0,0 +1,17 @@
+import { createI18n } from 'vue-i18n';
+
+import localePL from './locales/pl.json';
+import localeEN from './locales/en.json';
+
+const i18n = createI18n({
+  locale: 'pl',
+  fallbackLocale: 'pl',
+  legacy: false,
+
+  messages: {
+    pl: localePL,
+    en: localeEN,
+  },
+});
+
+export default i18n;
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
 
-import './style.css'
+import './style.css';
+import i18n from './i18n';
 
 const pinia = createPinia();
 
-createApp(App).use(pinia).mount('#app');
+createApp(App).use(i18n).use(pinia).mount('#app');
